e9d8872d8219b214b0407d0fb1805b2276ec9652
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-07-31 19:05:51 -0400

    Agregando los 3 Dto: ProductoDto CategoriaDto

---
+++ app/src/main/java/com/edu/ucne/proyectofinalap2_ronell/data/remote/dto/CategoriaDto.kt
@@ -0,0 +1,7 @@
+package com.edu.ucne.proyectofinalap2_ronell.data.remote.dto
+
+data class CategoriaDto(
+    val categoriaId: Int,
+    val nombreCategoria: String,
+    val descripcionCategoria: String,
+)
---
+++ app/src/main/java/com/edu/ucne/proyectofinalap2_ronell/data/remote/dto/ProductoDto.kt
@@ -0,0 +1,14 @@
+package com.edu.ucne.proyectofinalap2_ronell.data.remote.dto
+
+data class ProductoDto(
+    val productoId: Int,
+    val nombreProducto: String,
+    val descripcionProducto: String,
+    val fechaVencProducto: String,
+    val stockProducto: Int,
+//    val marcaProducto: String,
+//    val categoriaId: Int,
+//    val precioCompraProducto: Double,
+//    val precioVentaProducto: Double,
+//    val codigoBarrasProducto: String
+)
