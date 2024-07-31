7079023d13ae7495976e3209933510fcf356d9c1
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-07-31 19:10:44 -0400

    Llamando el InventarioNavHost en el MainActivity

--- app/src/main/java/com/edu/ucne/proyectofinalap2_ronell/MainActivity.kt
+++ app/src/main/java/com/edu/ucne/proyectofinalap2_ronell/MainActivity.kt
@@ -4,44 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.edu.ucne.proyectofinalap2_ronell.presentation.Home.Dashboard
+import com.edu.ucne.proyectofinalap2_ronell.presentation.navigation.InventarioNavHost
 import com.edu.ucne.proyectofinalap2_ronell.ui.theme.ProyectoFinalAP2_RonellTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ProyectoFinalAP2_RonellTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navHost = rememberNavController()
+                InventarioNavHost(navHostController = navHost)
+//                Dashboard()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProyectoFinalAP2_RonellTheme {
-        Greeting("Android")
     }
 }
